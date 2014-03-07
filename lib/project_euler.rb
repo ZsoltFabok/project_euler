5756686c5f1a99606ac62699dd57f26fda1b8d5c
Zsolt Fabok <me@zsoltfabok.com>
2014-03-07 08:59:00 +0100

    a bit better way to load problems

--- lib/project_euler.rb
+++ lib/project_euler.rb
@@ -1,5 +1,6 @@
-Dir.foreach(File.expand_path('../../lib/project_euler', __FILE__)) do |problem|
+module_name = "project_euler"
+Dir.foreach(File.expand_path("../../lib/#{module_name}", __FILE__)) do |problem|
   if problem =~ /(problem_[0-9])+\.rb/
-    require "project_euler/#{problem}"
+    require "#{module_name}/#{problem}"
   end
 end
