b7ff59f91f0a8060f5ad1293a406ee05ed54b441
Zsolt Fabok <me@zsoltfabok.com>
2014-03-11 07:01:23 +0100

    Fermat's little theorem produced pseudoprimes and I had to get rid of them from the result

--- lib/project_euler/math/prime.rb
+++ lib/project_euler/math/prime.rb
@@ -14,7 +14,6 @@ module Math
       primes
     end
 
-    protected
     def next_prime(number)
       number += 1
       while (!is_prime?(number)) do
@@ -25,7 +24,38 @@ module Math
 
     # uses Fermat's little theorem, with a = 2
     def is_prime?(number)
-      (2 ** number - 2) % number == 0
+      a = 2
+      if (a ** (number - 1) - 1) % number == 0
+        if is_pseudoprime?(number)
+          return false
+        end
+        return true
+      end
+      return  false
+    end
+
+    protected
+    def is_pseudoprime?(number)
+      number_sqrt = Math.sqrt(number).to_i
+      divisors = (1..number_sqrt).to_a.delete_if do |n|
+        is_composite_of?(n, [2, 3, 5, 7, 11])
+      end
+
+      divisors.each do |n|
+        if number % n == 0 && n > 1 # n > 1 makes sure that it works for actual primes in the recursion
+          return true
+        end
+      end
+      return false
+    end
+
+    def is_composite_of?(n, base_primes)
+      base_primes.each do |d|
+        if (n % d == 0) && ((n / d) > 1)
+          return true
+        end
+      end
+      false
     end
   end
 end
