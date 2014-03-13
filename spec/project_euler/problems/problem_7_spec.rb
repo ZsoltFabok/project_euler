e7e0a65513677cbfda89a83b49e8dc30ac7ee2c2
Zsolt Fabok <me@zsoltfabok.com>
2014-03-13 10:49:31 +0100

    minor fix in problem_7_spec

--- spec/project_euler/problems/problem_7_spec.rb
+++ spec/project_euler/problems/problem_7_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 describe Problems::Problem7 do
   subject(:problem) {Problems::Problem7.new}
 
-  it "does return 13 as the 6th prime number" do
+  it "returns 13 as the 6th prime number" do
     problem.execute(6).should eq 13
   end
 
-  it "does return 541 as the 100th prime number" do
+  it "returns 541 as the 100th prime number" do
     problem.execute(100).should eq 541
   end
 
-  it "does return 3581 as the 500th prime number" do
+  it "returns 3581 as the 500th prime number" do
     problem.execute(500).should eq 3571
   end
 
-  it "does return 7919 as the 1000th prime number" do
+  it "returns 7919 as the 1000th prime number" do
     problem.execute(1000).should eq 7919
   end
 
-  it "does return 104743 as the 10001st prime number" do
+  it "returns 104743 as the 10001st prime number" do
     problem.execute(10001).should eq 104743
   end
 end
