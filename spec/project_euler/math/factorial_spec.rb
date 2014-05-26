b4b300f6d4bcfb812718505660b0c52e85486fc6
Zsolt Fabok <me@zsoltfabok.com>
2014-05-26 19:34:50 +0200

    fix factorial test case

--- spec/project_euler/math/factorial_spec.rb
+++ spec/project_euler/math/factorial_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Math::Factorial do
+  subject(:factorial) {Math::Factorial.new}
+
   context "#get" do
     it "should return 1 for 1" do
-      Math::Factorial.get(1).should eq 1
+      factorial.get(1).should eq 1
     end
 
     it "should return 6 for 3" do
-      Math::Factorial.get(3).should eq 6
+      factorial.get(3).should eq 6
     end
   end
 end
